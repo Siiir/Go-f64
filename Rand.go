7c4b3958eddf3dccfd0db9ac11dae1cef3ef93b2
Siiir <88857181+Siiir@users.noreply.github.com>
2022-10-04 22:39:01 +0200

    Docs improvement.

--- Rand.go
+++ Rand.go
@@ -5,7 +5,7 @@ import (
 	"unsafe"
 )
 
-//Returns either `fl` or `-fl`
+// Returns either `fl` or `-fl` .
 func WithRandSign(fl float64) float64 {
 	if (rand.Int31() & 1) == 1 {
 		fl *= -1
@@ -13,14 +13,20 @@ func WithRandSign(fl float64) float64 {
 	return fl
 }
 
-/* Returns completly random float64.
-Might be abnormal or special.*/
+/*
+	Returns completly random float64.
+	Might be abnormal or special.
+*/
 func Rand() float64 {
 	i64 := rand.Uint64()
 	return *(*float64)(unsafe.Pointer(&i64))
 }
 
-/*Returns random, not-abnormal, not-special (not NaN, not Inf) `float64`*/
+/*
+	Returns random,
+	not-abnormal, not-special (not NaN, not Inf),
+	`float64`
+*/
 func RandNormal() float64 {
 	var u64 uint64
 	for {
--- Relations.go
+++ Relations.go
@@ -2,7 +2,10 @@ package f64
 
 import "math"
 
-// Equivalent to: (math.IsNaN(fl1) && math.IsNaN(fl2)) || fl1 == fl2
+/*
+	Description:
+		return (math.IsNaN(fl1) && math.IsNaN(fl2)) || fl1 == fl2
+*/
 func ValEquivalence(fl1, fl2 float64) bool {
 	return fl1 == fl2 || (math.IsNaN(fl1) && math.IsNaN(fl2))
 }
